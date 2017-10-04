c0c4f1a7ae4cb4acf65dc783d5e752ab0a09352a
Disrupt-MushfiQ <hello@mrshishir.me>
2017-10-04 17:31:27 +0600

    Added Nodemailer
    Added Nodemailer

--- apiServer/src/services/mailer/mailer-hooks.js
+++ apiServer/src/services/mailer/mailer-hooks.js
@@ -2,7 +2,41 @@
 'use strict';
 const sgMail = require('@sendgrid/mail');
 const mailSettings = require('./mail-settings');
+const nodemailer = require('nodemailer');
 
+function sendMail(emailProps) {
+
+    // Create the transporter with the required configuration for Gmail
+    // change the user and pass !
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.zoho.com',
+        port: 465,
+        secure: true, // use SSL
+        auth: {
+            user: 'myzoho@zoho.com',
+            pass: 'myPassword'
+        }
+    });
+
+    // setup e-mail data, even with unicode symbols
+    var mailOptions = {
+        from: '"Our Code World " <myzoho@zoho.com>', // sender address (who sends)
+        to: 'mymail@mail.com, mymail2@mail.com', // list of receivers (who receives)
+        subject: 'Hello ', // Subject line
+        text: 'Hello world ', // plaintext body
+        html: '<b>Hello world </b><br> This is the first email sent with Nodemailer in Node.js' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log('Message sent: ' + info.response);
+    });
+
+}
 
 module.exports = {
     before: {
@@ -22,16 +56,38 @@ module.exports = {
         create: [
             function (hooks) {
 
-                sgMail.setApiKey(mailSettings.apiKey);
-                console.log(hooks.data);
-                const msg = {
-                    to: hooks.data.email,
-                    from: 'hello@mushfiqurs.com',
-                    subject: hooks.data.emailSubject,
-                    text: hooks.data.emailInText,
-                    html: hooks.data.emailInHTML,
+                //sendMail('OK');
+
+                // Create the transporter with the required configuration for Gmail
+                // change the user and pass !
+                var transporter = nodemailer.createTransport({
+                    host: 'smtp.zoho.com',
+                    port: 465,
+                    secure: true, // use SSL
+                    auth: {
+                        user: 'hello@mushfiqurs.com',
+                        pass: 'dntDARE@here'
+                    }
+                });
+
+                // setup e-mail data, even with unicode symbols
+                var mailOptions = {
+                    from: '"mushfiqurs blog" <hello@mushfiqurs.com>', // sender address (who sends)
+                    to: hooks.data.email, // list of receivers (who receives)
+                    subject: hooks.data.emailSubject, // Subject line
+                    text: hooks.data.emailInText, // plaintext body
+                    html: hooks.data.emailInHTML // html body
                 };
-                sgMail.send(msg);
+
+                // send mail with defined transport object
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        return console.log(error);
+                    }
+
+                    console.log('Message sent: ' + info.response);
+                });
+
             }
         ],
         update: [],
